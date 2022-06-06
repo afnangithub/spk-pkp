81d1af4550289274d38e53a7cfcd645a4efcdf7d
AM <fadajaka@gmail.com>
2022-06-07 06:57:44 +0700

    repair model

--- app/Models/AnalisisModel.php
+++ app/Models/AnalisisModel.php
@@ -1,6 +1,5 @@
 <?php
 namespace App\Models;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 class AnalisisModel extends Model
